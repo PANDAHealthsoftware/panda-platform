0ebfc3cbcd2cf8e9a9b81d07ec83958cd38928a1
Simon <simon@jigsawfuel.com>
2025-04-03 09:59:51 +0100

    Updated CreateAppointmentDtoValidator.cs

--- PANDA.Api/Validation/CreateAppointmentDtoValidator.cs
+++ PANDA.Api/Validation/CreateAppointmentDtoValidator.cs
@@ -1,25 +1,32 @@
 ﻿using FluentValidation;
+using PANDA.Api.Common;
 using PANDA.Api.Dto;
 
-namespace PANDA.Api.Validation
+namespace PANDA.Api.Validation;
+
+public class CreateAppointmentDtoValidator : AbstractValidator<CreateAppointmentDto>
 {
-    public class CreateAppointmentDtoValidator : AbstractValidator<CreateAppointmentDto>
+    public CreateAppointmentDtoValidator()
     {
-        public CreateAppointmentDtoValidator()
-        {
-            RuleFor(x => x.PatientId).GreaterThan(0).WithMessage("PatientId is required.");
-            RuleFor(x => x.AppointmentDate)
-                .GreaterThan(DateTimeOffset.UtcNow)
-                .WithMessage("Appointment must be scheduled in the future.");
-            RuleFor(x => x.Status)
-                .IsInEnum()
-                .WithMessage("Invalid appointment status.");
-            RuleFor(x => x.Clinician)
-                .NotEmpty()
-                .MaximumLength(100);
-            RuleFor(x => x.Department)
-                .NotEmpty()
-                .MaximumLength(100);
-        }
+        RuleFor(x => x.PatientId)
+            .GreaterThan(0)
+            .WithMessage("PatientId is required.");
+
+        RuleFor(x => x.AppointmentDate)
+            .GreaterThan(DateTimeOffset.UtcNow)
+            .WithMessage("Appointment must be scheduled in the future.");
+
+        RuleFor(x => x.Status)
+            .IsInEnum()
+            .WithMessage(ErrorMessages.InvalidAppointmentStatus);
+
+        RuleFor(x => x.Clinician)
+            .NotEmpty()
+            .MaximumLength(100)
+            .WithMessage("Clinician name is required and must be 100 characters or fewer.");
+
+        RuleFor(x => x.Department)
+            .IsInEnum()
+            .WithMessage("Invalid department.");
     }
 }
